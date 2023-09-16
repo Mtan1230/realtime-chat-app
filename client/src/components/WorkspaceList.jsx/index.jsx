8aaa5580bfc146901441489ae6e20b9403aa5ad6
Mtan1230 <m.tan1230@gmail.com>
2023-09-16 02:23:08 -0400

    add workspace list to homepage

--- client/src/components/WorkspaceList.jsx/index.jsx
+++ client/src/components/WorkspaceList.jsx/index.jsx
@@ -1,5 +1,4 @@
 import { useQuery } from '@apollo/client';
-import { useState } from 'react';
 
 import { QUERY_ME } from '../../utils/queries';
 import CreateWorkspace from '../CustomModals/CreateWorkspace';
@@ -9,14 +8,21 @@ const WorkspaceList = () => {
   const { loading, data } = useQuery(QUERY_ME);
   console.log(data);
 
-
   return (
     <>
-      {loading ? <h1>loading...</h1> : <ListGroup>
-        {data?.me.workspaces.map(w => {
-          return <p key={w.name}>{w.name}</p>
-        })}
-        </ListGroup>}
+      {loading ? (
+        <h1>loading...</h1>
+      ) : (
+        <ListGroup>
+          {data?.me.workspaces.map((w) => {
+            return (
+              <ListGroup.Item action href={`/${w._id}`} key={w.name}>
+                {w.name}
+              </ListGroup.Item>
+            );
+          })}
+        </ListGroup>
+      )}
       <CreateWorkspace />
     </>
   );
